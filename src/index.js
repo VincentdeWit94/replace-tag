ace34e89b291c5a4467d981e19e9e2e5387c7a04
Vincent de Wit <vincent@flex-appeal.nl>
2017-04-04 17:05:00 +0200

    Second commit, changed regex and stuff

--- src/index.js
+++ src/index.js
@@ -27,17 +27,17 @@ if(program.folder){
   fs.readdir(program.folder, (err, files) => {
     files.forEach(file => {
 
-      console.log('Reading file:', file);
+      var file = program.folder + "/" + file;
+      console.log('Trying to open file:', file);
       fs.readFile(file, 'utf8', function (err,data) {
         if (err) {
           return console.log(err);
         }
 
-        var regex = new RegExp("(?<=:"+program.tag+":)(.*)(?=:)","g");
+        var regex = new RegExp("(?:^|\s):("+program.tag+"):(.*?)(?:\s|$)/gm");
         var parsedPath = path.parse(file);
         if (parsedPath) {
-          var result = data.replace(regex, parsedPath.name);
-
+          var result = data.replace(regex, "$1"+parsedPath.name);
           fs.writeFile(file, result, 'utf8', function (err) {
              if (err) return console.log(err);
           });
